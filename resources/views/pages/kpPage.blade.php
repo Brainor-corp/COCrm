0559d2042c9e77bdf90d92dc3516a4147297a374
peresekinss <peresekinss@gmail.com>
2019-02-18 17:11:55 +0300

    Правки верстки

--- resources/views/pages/kpPage.blade.php
+++ resources/views/pages/kpPage.blade.php
@@ -3,17 +3,32 @@
 @section('content')
 
     <div class="container">
-        <div class="offerGroup-name px-4 py-2">
-            <div class="row">
-                <div class="col-8">
-                    <h3>
-                        Коммерческое предложение {{ $offersGroup->name }}
-                    </h3>
+        <div class="px-4 py-2">
+            <div class="row head-bg">
+                <div class="col-3 mt-3">
+                    <img class="img-fluid" src="{{ asset('images/logo.png') }}" alt="">
                 </div>
-                <div class="col-4 text-right">
-                    <h3>
-                        {{ \Carbon\Carbon::parse($offersGroup->created_at)->format('d.m.Y') }}
-                    </h3>
+                <div class="col-5 mt-3">
+                    <p class="mb-0 font-weight-bold h5">Продажа и установка систем безопасности</p>
+                    <p class="mb-0 font-weight-bold h5">в Москве и Московской области</p>
+                </div>
+                <div class="col-4 mt-3">
+                    <div class="col-12 float-right">
+                        <p class="mb-0 text-white h4 text-right float-right"><img class="svg-icon mx-2" src="{{ asset('images/svg/phone-solid.svg') }}" alt="phone">+7(495)003-45-62</p>
+                    </div>
+                    <div class="float-right">
+                        <p class="mb-0 text-white h4 text-right"><img class="svg-icon mx-2" src="{{ asset('images/svg/envelope-regular.svg') }}" alt="email">zapros@aronasb.ru</p>
+                    </div>
+                </div>
+            </div>
+            <div class="head-bg-bottom row align-items-center">
+                <p class="font-weight-bold mt-1"><img class="svg-icon mx-2" src="{{ asset('images/svg/map-marker-alt-solid.svg') }}" alt="address">231123, Москва, 3-й проезд Перова поля, дом 8</p>
+            </div>
+            <div class="row">
+                <div class="col-12 my-4">
+                    <h1 class="font-weight-bold">
+                        Коммерческое предложение <span class="text-primary">{{ $offersGroup->name }}</span>
+                    </h1>
                 </div>
             </div>
         </div>
@@ -22,14 +37,16 @@
 
             <div class="offer my-5">
                 <div class="offer-name mt-3">
-                    <h4>
+                    <h3>
                         {{ $offer->name }}
-                    </h4>
+                        <span class="text-right">{{ \Carbon\Carbon::parse($offersGroup->created_at)->format('d.m.Y') }}</span>
+                    </h3>
                 </div>
-                <table class="table table-sm">
-                    <thead>
+                <div class="check-border p-3 my-4">
+                    <table class="table table-sm">
+                        <thead>
                         <tr>
-                            <th colspan="6" class="bg-gray">1. Оборудование и расходные материалы</th>
+                            <th colspan="6" class="border-top-0 border-bottom-0">1. Оборудование и расходные материалы</th>
                         </tr>
                         <tr>
                             <th scope="col" colspan="3">Наименование</th>
@@ -37,8 +54,8 @@
                             <th scope="col" nowrap class="text-center">Цена, руб.</th>
                             <th scope="col" nowrap class="text-center">Всего, руб.</th>
                         </tr>
-                    </thead>
-                    <tbody>
+                        </thead>
+                        <tbody>
                         @php($totalEquipmentSum = 0)
                         @php($consumableSum = 0)
                         @foreach($offer->equipments as $equipment)
@@ -65,52 +82,55 @@
                             <td class="align-middle text-center">{{ $consumableSum }}</td>
                         </tr>
                         <tr>
-                            <td colspan="6" class="bg-light-blue text-right"><h4>Всего за оборудование {{ $totalEquipmentSum + $consumableSum }}р.</h4></td>
+                            <td colspan="6" class="check-in-card-border my-4 text-right"><h4>Всего за оборудование {{ $totalEquipmentSum + $consumableSum }}р.</h4></td>
                         </tr>
-                    </tbody>
-                </table>
-                <table class="table table-sm">
-                    <thead>
-                    <tr>
-                        <th colspan="5" class="bg-gray">2. Монтажные и пуско-наладочные работы</th>
-                    </tr>
-                    <tr>
-                        <th scope="col" colspan="2">Наименование работ</th>
-                        <th scope="col" nowrap class="text-center">Ед. изм.</th>
-                        <th scope="col" nowrap class="text-center">Кол-во</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @php($totalWorkSum = 0)
-                        @foreach($offer->equipments as $equipment)
-                            @if($equipment->class == 'work')
-                                @php($totalWorkSum += $equipment->pivot->price * $equipment->pivot->quantity)
-                                <tr>
-                                    <td class="align-middle">{{ $equipment->name }}</td>
-                                    <td class="align-middle text-center"></td>
-                                    <td class="align-middle text-center">{{ $equipment->points }}</td>
-                                    <td class="align-middle text-center">{{ $equipment->pivot->quantity }}</td>
-                                </tr>
-                            @endif
-                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="check-border p-3 my-4">
+                    <table class="table table-sm">
+                        <thead>
                         <tr>
-                            <td colspan="5" class="bg-beige text-right"><h4>Всего за работы ( с НДС ): {{ $totalWorkSum }}р.</h4></td>
+                            <th colspan="5" class="bg-gray">2. Монтажные и пуско-наладочные работы</th>
                         </tr>
                         <tr>
-                            <td colspan="5" class="bg-light-blue text-right"><h4>Всего за работы ( без НДС, Доп. скидка - ??? ): ???р.</h4></td>
+                            <th scope="col" colspan="2">Наименование работ</th>
+                            <th scope="col" nowrap class="text-center">Ед. изм.</th>
+                            <th scope="col" nowrap class="text-center">Кол-во</th>
                         </tr>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @php($totalWorkSum = 0)
+                            @foreach($offer->equipments as $equipment)
+                                @if($equipment->class == 'work')
+                                    @php($totalWorkSum += $equipment->pivot->price * $equipment->pivot->quantity)
+                                    <tr>
+                                        <td class="align-middle">{{ $equipment->name }}</td>
+                                        <td class="align-middle text-center"></td>
+                                        <td class="align-middle text-center">{{ $equipment->points }}</td>
+                                        <td class="align-middle text-center">{{ $equipment->pivot->quantity }}</td>
+                                    </tr>
+                                @endif
+                            @endforeach
+                            <tr>
+                                <td colspan="5" class="check-in-card-border my-4 text-right"><h4>Всего за работы ( с НДС ): {{ $totalWorkSum }}р.</h4></td>
+                            </tr>
+                            <tr>
+                                <td colspan="5" class="check-in-card-border my-4 text-right"><h4>Всего за работы ( без НДС, Доп. скидка - <span class="text-danger">???</span> ): ???р.</h4></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
             <hr>
         @endforeach
 
-        <table class="table table-sm">
+        <table class="table table-borderless">
             <tr>
-                <td colspan="5" class="bg-beige text-right"><h4>Общая стоимость ( 1 договор с НДС ): ???р.</h4></td>
+                <td class="text-right my-4 font-weight-bold"><h4>Общая стоимость ( 1 договор с НДС ): ???р.</h4></td>
             </tr>
             <tr>
-                <td colspan="5" class="bg-light-blue text-right"><h4>Общая стоимость ( 2 договора - оборудование с НДС, работы без НДС ): ???р.</h4></td>
+                <td class="text-right my-4 font-weight-bold"><h4>Общая стоимость ( 2 договора - оборудование с НДС, работы без НДС ): ???р.</h4></td>
             </tr>
         </table>
     </div>
